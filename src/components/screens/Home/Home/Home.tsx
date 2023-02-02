0eb88a4b0afed051ea3636ac46425cc698951deb
Esin Andrew <kubanesin@gmail.com>
2023-02-02 20:38:44 +0300

    fix:исправил шибки

--- src/components/screens/Home/Home/Home.tsx
+++ src/components/screens/Home/Home/Home.tsx
@@ -8,74 +8,74 @@ import classes from './Home.module.css';
 type TodofilterType = 'all' | 'active' | 'completed';
 
 const Home = () => {
-  const [todos, setTodos] = React.useState([
-    { id: 1, title: 'Изучить React', isCompleted: false },
-    { id: 2, title: 'Изучить TypeScript', isCompleted: false },
-    { id: 3, title: 'Изучить Redux', isCompleted: false },
-  ]);
+   const [todos, setTodos] = React.useState([
+      { id: 1, title: 'Изучить React', isCompleted: false },
+      { id: 2, title: 'Изучить TypeScript', isCompleted: false },
+      { id: 3, title: 'Изучить Redux', isCompleted: false },
+   ]);
 
-  const [filter, setFilter] = React.useState<TodofilterType>('all');
+   const [filter, setFilter] = React.useState<TodofilterType>('all');
 
-  let filterTodo = todos;
+   let filterTodo = todos;
 
-  if (filter === 'completed') {
-    filterTodo = todos.filter((t) => t.isCompleted === true);
-  }
+   if (filter === 'completed') {
+      filterTodo = todos.filter((t) => t.isCompleted === true);
+   }
 
-  if (filter === 'active') {
-    filterTodo = todos.filter((t) => t.isCompleted === false);
-  }
+   if (filter === 'active') {
+      filterTodo = todos.filter((t) => t.isCompleted === false);
+   }
 
-  const filterTodoForList = (value: TodofilterType) => {
-    setFilter(value);
-  };
+   const filterTodoForList = (value: TodofilterType) => {
+      setFilter(value);
+   };
 
-  const saveLocalStorageTodo = (newTodo: any) => {
-    localStorage.setItem('newTodo', JSON.stringify(newTodo));
-    setTodos(newTodo);
-  };
+   const saveLocalStorageTodo = (newTodo: any) => {
+      localStorage.setItem('newTodo', JSON.stringify(newTodo));
+      setTodos(newTodo);
+   };
 
-  React.useEffect(() => {
-    const getTodoItem = localStorage.getItem('newTodo');
-    if (getTodoItem) {
-      setTodos(JSON.parse(getTodoItem));
-    }
-  }, []);
-
-  const removeTodo = (id: number) => {
-    saveLocalStorageTodo(todos.filter((t) => t.id !== id));
-  };
-
-  const checkedTodo = (id: number) => {
-    const checkedTodo = todos.map((item) => {
-      if (item.id === id) {
-        return {
-          ...item,
-          isCompleted: !item.isCompleted,
-        };
-      } else {
-        return item;
+   React.useEffect(() => {
+      const getTodoItem = localStorage.getItem('newTodo');
+      if (getTodoItem) {
+         setTodos(JSON.parse(getTodoItem));
       }
-    });
-    saveLocalStorageTodo(checkedTodo);
-    //setTodos(prevState=> prevState.map(item=>item.id === id ? {...item, isCompleted: !item.isCompleted} : item))
-  };
+   }, []);
 
-  return (
-    <div className={classes.page}>
-      <h1 className={classes.page__title}>Список дел.</h1>
+   const removeTodo = (id: number) => {
+      saveLocalStorageTodo(todos.filter((t) => t.id !== id));
+   };
 
-      <FilterTodoItem filterTodoForList={filterTodoForList} />
+   const checkedTodo = (id: number) => {
+      const checkedTodo = todos.map((item) => {
+         if (item.id === id) {
+            return {
+               ...item,
+               isCompleted: !item.isCompleted,
+            };
+         } else {
+            return item;
+         }
+      });
+      saveLocalStorageTodo(checkedTodo);
+      //setTodos(prevState=> prevState.map(item=>item.id === id ? {...item, isCompleted: !item.isCompleted} : item))
+   };
 
-      <CreateTodoItem
-        todos={todos}
-        setTodos={setTodos}
-        saveLocalStorageTodo={saveLocalStorageTodo}
-      />
+   return (
+      <div className={classes.page}>
+         <h1 className={classes.page__title}>Список задач.</h1>
 
-      <TodoItem todos={filterTodo} removeTodo={removeTodo} checkedTodo={checkedTodo} />
-    </div>
-  );
+         <FilterTodoItem filterTodoForList={filterTodoForList} />
+
+         <CreateTodoItem
+            todos={todos}
+            setTodos={setTodos}
+            saveLocalStorageTodo={saveLocalStorageTodo}
+         />
+
+         <TodoItem todos={filterTodo} removeTodo={removeTodo} checkedTodo={checkedTodo} />
+      </div>
+   );
 };
 
 export default Home;
